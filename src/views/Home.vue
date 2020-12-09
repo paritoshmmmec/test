96ae68eb8919a2ac83e968b2c01f7d5c2095f544
paritosh baghel <paritoshmmmec@gmail.com>
2020-12-09 08:14:20 -0500

    Update another

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <section class="section">
+      <b-table :data="data" :columns="columns"> </b-table>
+    </section>
   </div>
 </template>
 
@@ -10,9 +11,66 @@
 //import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     // HelloWorld
-  }
-}
+  },
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          first_name: "Jesse",
+          last_name: "Simmons",
+          date: "2016-10-15 13:43:27",
+          gender: "Male",
+        },
+        {
+          id: 2,
+          first_name: "John",
+          last_name: "Jacobs",
+          date: "2016-12-15 06:00:53",
+          gender: "Male",
+        },
+        {
+          id: 3,
+          first_name: "Tina",
+          last_name: "Gilbert",
+          date: "2016-04-26 06:26:28",
+          gender: "Female",
+        },
+        {
+          id: 4,
+          first_name: "Clarence",
+          last_name: "Flores",
+          date: "2016-04-10 10:28:46",
+          gender: "Male",
+        },
+        {
+          id: 5,
+          first_name: "Anne",
+          last_name: "Lee",
+          date: "2016-12-06 14:38:38",
+          gender: "Female",
+        },
+      ],
+      columns: [
+        {
+          field: "first_name",
+          label: "First Name",
+          searchable: true,
+        },
+        {
+          field: "last_name",
+          label: "Last Name",
+          searchable: true,
+        },
+        {
+          field: "gender",
+          label: "Gender",
+        },
+      ],
+    };
+  },
+};
 </script>
