f0b5e61e1bc814cd2fa1215a44beae766ad59b52
paritosh baghel <paritoshmmmec@gmail.com>
2020-12-09 04:40:51 -0500

    Testing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">WayPI</h1>
+          <h2 class="subtitle">An API Catalog</h2>
+        </div>
+      </div>
+    </section>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/views/AddApi.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <b-field label="Enter the swagger url">
+          <b-input placeholder="Endpoint Url" v-model="endpoint" expanded></b-input>
+          <p class="control">
+            <b-button v-on:click="parse" class="button is-primary">Parse </b-button>
+          </p>
+        </b-field>
+      </div>
+    </section>
+    <div v-for="e in endpoints" :key="e.id">
+      <nav class="panel spacing">
+        <p class="panel-heading">
+          Endpoint: {{ e.endpointUrl }} <b-tag>{{ e.verb }}</b-tag>
+        </p>
+        <div class="panel-block is-active">
+          <div class="container">
+            <b-field label="Team Name">
+              <b-input placeholder="Team Name" v-model="e.teamName" expanded></b-input>
+            </b-field>
+            <b-field label="Slack Handle">
+              <b-input
+                placeholder="Slack Handle"
+                v-model="e.slackHandle"
+                expanded
+              ></b-input>
+            </b-field>
+            <b-field label="Description">
+              <b-input
+                placeholder="Description"
+                v-model="e.description"
+                type="textarea"
+                expanded
+              ></b-input>
+            </b-field>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="spacing">
+      <button class="button is-link is-outlined is-fullwidth" v-on:click="Save">
+        Save
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  data() {
+    const endpoints = [
+      {
+        id: 1,
+        base: "https://petstore.swagger.io/v2/",
+        endpointUrl: "pet/{petId}/uploadImage",
+        verb: "POST",
+        teamName: "",
+        description: "",
+        slackHandle: "",
+      },
+      {
+        id: 2,
+        base: "https://petstore.swagger.io/v2/",
+        endpointUrl: "pet/findByStatus",
+        verb: "GET",
+        teamName: "",
+        description: "",
+        slackHandle: "",
+      },
+    ];
+    return {
+      labelPosition: "on-border",
+      endpoint: "",
+      endpoints,
+      showDetailIcon: true,
+    };
+  },
+  components: {
+    // HelloWorld
+  },
+  methods: {
+    parse: function () {
+      console.log(this.endpoint);
+      console.log(this.$router);
+    },
+    Save: function () {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+<style scoped>
+.spacing {
+  margin: 20px;
+}
+</style>
